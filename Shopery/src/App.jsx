cd78438e7e9d35ba519a140c74278bf41002676f
NaGit09 <nhutanhtran806@gmail.com>
2025-02-25 09:04:34 +0700

    init project

---
+++ Shopery/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <h1 className='text-center text-primary'>Hello world</h1>
+       </div>
+    </>
+  )
+}
+
+export default App
